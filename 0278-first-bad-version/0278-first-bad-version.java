9722387768a24548e784aed7c5744db2b2ca4278
Rakshit Gupta <rakshitgupta5453@gmail.com>
2026-07-15 19:42:50 +0530

    Time: 23 ms (5.05%), Space: 42.2 MB (35.23%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.java
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -7,10 +7,10 @@ public class Solution extends VersionControl {
         int start = 1;
         int end = n;
 
-        while(start < end){
+        while(start <= end){
             int mid = start + (end - start)/2;
             if(isBadVersion(mid) == true){
-                end = mid;
+                end = mid - 1;
             }
             else if(isBadVersion(mid) == false){
                 start = mid + 1;
